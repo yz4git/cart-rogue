8c026bf86bf38e40fa12033ad2f38ab81b4f2df5
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-16 01:26:00 +0900

    Enable fullscreen presentation helper

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import CartRogueGame from "./CartRogueGamePhase13";
+import FullscreenPresentation from "./FullscreenPresentation";
 import ServiceWorkerRegistration from "./ServiceWorkerRegistration";
 
 export default function Page() {
   return <>
     <ServiceWorkerRegistration />
+    <FullscreenPresentation />
     <CartRogueGame />
   </>;
 }
