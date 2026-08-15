6471b6c7188186d7c60572d8f13a82b89607a7c9
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-15 16:51:24 +0900

    Enable Phase 13 visual layer

---
+++ app/CartRogueGamePhase13.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import "../src/cart/CartRoguePhase13Visuals";
+import CartRogueGame from "./CartRogueGame";
+
+export default function CartRogueGamePhase13() {
+  return <CartRogueGame />;
+}
