7ab69af12f8fa179cb6545147ffb445a5fc39dbc
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-15 18:25:57 +0900

    Enable Phase 14 curved floor surface pass

--- app/CartRogueGamePhase13.tsx
+++ app/CartRogueGamePhase13.tsx
@@ -3,6 +3,7 @@
 import "../src/cart/CartRoguePhase13Visuals";
 import "../src/cart/CartRoguePhase13Grade";
 import "../src/cart/CartRoguePhase14Arenas";
+import "../src/cart/CartRoguePhase14SurfacePass";
 import CartRogueGame from "./CartRogueGame";
 
 export default function CartRogueGamePhase13() {
