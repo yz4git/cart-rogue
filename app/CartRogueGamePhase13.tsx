a0f415445a6e7d26a57acee7b8b60744210beaee
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-15 16:54:59 +0900

    Apply Phase 13 color grade and framing

--- app/CartRogueGamePhase13.tsx
+++ app/CartRogueGamePhase13.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import "../src/cart/CartRoguePhase13Visuals";
+import "../src/cart/CartRoguePhase13Grade";
 import CartRogueGame from "./CartRogueGame";
 
 export default function CartRogueGamePhase13() {
