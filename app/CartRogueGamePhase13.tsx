53ea3667e16d33768b26c402a322e29d576c45ae
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-15 22:36:22 +0900

    feat: enable Phase 19 target-art pass

--- app/CartRogueGamePhase13.tsx
+++ app/CartRogueGamePhase13.tsx
@@ -9,6 +9,7 @@ import "../src/cart/CartRoguePhase16Flow";
 import "../src/cart/CartRoguePhase17CombatEvolution";
 import "../src/cart/CartRoguePhase18VisualOverdrive";
 import "../src/cart/CartRoguePhase18VisualPolish";
+import "../src/cart/CartRoguePhase19TargetArt";
 import CartRogueGame from "./CartRogueGame";
 
 export default function CartRogueGamePhase13() {
