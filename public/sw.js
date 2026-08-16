90852dc97e8287eb52936c9fa062c069524b9825
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-17 01:13:45 +0900

    Bump Cart Rogue PWA cache to v20

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@ const BASE_PATH = self.location.pathname
   .replace(/\/$/, "");
 const ROOT = `${BASE_PATH}/`;
 const CACHE_PREFIX = "voxel-rally-";
-const CACHE_VERSION = "v19";
+const CACHE_VERSION = "v20";
 const CACHE = `${CACHE_PREFIX}${CACHE_VERSION}`;
 const APP_SHELL = [ROOT, `${BASE_PATH}/manifest.json`, `${BASE_PATH}/favicon.svg`];
 
@@ -39,18 +39,11 @@ self.addEventListener("fetch", (event) => {
     );
     return;
   }
+
   event.respondWith(
-    caches.match(event.request).then((cached) =>
-      cached ??
-      fetch(event.request)
-        .then((response) => {
-          if (response.ok && new URL(event.request.url).origin === self.location.origin) {
-            const copy = response.clone();
-            void caches.open(CACHE).then((cache) => cache.put(event.request, copy));
-          }
-          return response;
-        })
-        .catch(() => caches.match(ROOT)),
-    ),
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+      if (response.ok) void caches.open(CACHE).then((cache) => cache.put(event.request, response.clone()));
+      return response;
+    })),
   );
 });
