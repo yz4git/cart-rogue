b7e7293f8c64cf0c774aa262405ff0740e240afa
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-19 08:41:18 +0900

    Add title and pause screens

--- app/CartRogueGamePhase13.tsx
+++ app/CartRogueGamePhase13.tsx
@@ -1,14 +1,22 @@
 "use client";
 
+import { useState } from "react";
 import "../src/cart/CartRogueRuntime";
+import "../src/cart/CartGameMenuRuntime";
 import CartRogueGame from "./CartRogueGame";
 import CartTurboHuntHudOverlay from "./CartTurboHuntHudOverlay";
 import CartCombatReadabilityPass from "./CartCombatReadabilityPass";
+import CartGameMenu from "./CartGameMenu";
 
 export default function CartRogueGamePhase13() {
+  const [started, setStarted] = useState(false);
+
   return <>
-    <CartRogueGame />
-    <CartTurboHuntHudOverlay />
-    <CartCombatReadabilityPass />
+    {started && <>
+      <CartRogueGame />
+      <CartTurboHuntHudOverlay />
+      <CartCombatReadabilityPass />
+    </>}
+    <CartGameMenu started={started} onStart={() => setStarted(true)} />
   </>;
 }
