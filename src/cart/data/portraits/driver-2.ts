a8f9e10abdc61f64f43dcb57a0a6f9ea3db66fc1
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-19 21:48:25 +0900

    Remove superseded long portrait chunk

--- src/cart/data/portraits/driver-2.ts
+++
@@ -1,2 +0,0 @@
-const chunk = "voMTTY//wuo6Uasavz88aqv/OCUwnjPzmC9sMN6yrIFjqijeR+FAej3GB+SIaBHvvVsODXlqBk8eSPo9vgubtSrac0VGYtenKo/pwgmM7B1zfP5yqbjegQ8mBMDjsv3ZIM75ODOvtaoLCNMKqgE=";
-export default chunk;
