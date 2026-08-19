59a85a328c47aeaf30e4390cd472db9533563c20
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-19 21:34:20 +0900

    Use uploaded Face Editor portraits in Phase102

---
+++ src/cart/CartRoguePhase102FaceImages.ts
@@ -0,0 +1,62 @@
+import {
+  registerCartCutinImagePortrait,
+  type CartFaceEditorExpressionId,
+} from "./CartRoguePhase102AnimeCutin";
+import driver0 from "./data/portraits/driver-0";
+import driver1 from "./data/portraits/driver-1";
+import driver2 from "./data/portraits/driver-2";
+import operator0 from "./data/portraits/operator-0";
+import operator1 from "./data/portraits/operator-1";
+import operator2 from "./data/portraits/operator-2";
+
+const EXPRESSIONS: readonly CartFaceEditorExpressionId[] = [
+  "neutral",
+  "smile",
+  "happy",
+  "angry",
+  "sad",
+  "surprised",
+  "serious",
+  "blink",
+] as const;
+
+export const CART_DRIVER_PORTRAIT_BASE64 = `${driver0}${driver1}${driver2}`;
+export const CART_OPERATOR_PORTRAIT_BASE64 = `${operator0}${operator1}${operator2}`;
+
+export const CART_PHASE102_FACE_IMAGE_META = {
+  driver: {
+    sourceSha256: "ff60e3c3d93f8e421003a7474962aca8ee0739ec68cba7becd9dbff74cadc0a1",
+    portraitSha256: "ac1c7a9d55bb58781ed8ae72c8b54922ee4c045e9a3a58ae9264188e877072f4",
+    sourceFormat: "face-editor-polygon-character",
+    baseStyle: "male",
+    hairStyle: "twin-tail",
+    faceShape: "diamond",
+    eyeStyle: "round",
+    mouthStyle: "frown",
+  },
+  operator: {
+    sourceSha256: "ca0a8b8e0e6823a0056bad8738612d0b8e02d260852575fa3c42438584cc1a99",
+    portraitSha256: "0ac5cefee65cebe8120c5de88b0e07a777be4a28c17391e44f444ecbcff7617c",
+    sourceFormat: "face-editor-polygon-character",
+    baseStyle: "female",
+    hairStyle: "wavy",
+    faceShape: "round",
+    eyeStyle: "side-glance",
+    mouthStyle: "smile",
+  },
+} as const;
+
+function dataUri(base64: string): string {
+  return `data:image/webp;base64,${base64}`;
+}
+
+export function installCartRoguePhase102FaceImages(): void {
+  const driver = dataUri(CART_DRIVER_PORTRAIT_BASE64);
+  const operator = dataUri(CART_OPERATOR_PORTRAIT_BASE64);
+  for (const expression of EXPRESSIONS) {
+    registerCartCutinImagePortrait("driver", expression, driver);
+    registerCartCutinImagePortrait("operator", expression, operator);
+  }
+}
+
+installCartRoguePhase102FaceImages();
